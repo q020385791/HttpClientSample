e7b386425f9db052eb91be596aefd1af94ad6bf0
sai wanza <q020385791@gmail.com>
2021-03-16 09:49:25 +0800

    加入專案檔案。

---
+++ HttpClientSample/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace HttpClientSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Post = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Post
+            // 
+            this.Post.Location = new System.Drawing.Point(190, 65);
+            this.Post.Name = "Post";
+            this.Post.Size = new System.Drawing.Size(103, 41);
+            this.Post.TabIndex = 0;
+            this.Post.Text = "Post";
+            this.Post.UseVisualStyleBackColor = true;
+            this.Post.Click += new System.EventHandler(this.Post_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Get";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 149);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Post);
+            this.Name = "Form1";
+            this.Text = "HttpClientTest";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Post;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ HttpClientSample/Form1.cs
@@ -0,0 +1,111 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HttpClientSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private  async void Post_Click(object sender, EventArgs e)
+        {
+            //目標api網址
+            string TargetUri = "";
+            
+            //資料結構包裝
+            Model _Model = new Model();
+
+            HttpClient client = new HttpClient();
+
+            #region 資料輸入
+            _Model.MProp1 = "This is Prop1";
+            _Model.MProp2 = "This is Prop2";
+            _Model.Data.ModelProp1 = "This is ModelProp1";
+            _Model.Data.ModelProp2 = "This is ModelProp2";
+            _Model.lstData.Add(_Model.Data);
+            _Model.lstData.Add(_Model.Data);
+            _Model.lstData.Add(_Model.Data);
+
+            #endregion
+            //資料轉為Json格式文字 再轉為byteArraycontent
+            var content = JsonConvert.SerializeObject(_Model);
+            var buffer = System.Text.Encoding.UTF8.GetBytes(content);
+            var byteContent = new ByteArrayContent(buffer);
+
+            var request = new HttpRequestMessage
+            {
+                //設定為Post
+                Method = HttpMethod.Post,
+                //設定目標網址
+                RequestUri = new Uri(TargetUri)
+            };
+            //資料輸入
+            request.Content = new StringContent(content,
+                                    Encoding.UTF8,
+                                    "application/json");
+
+            #region Header
+            //掛上自訂標頭
+            request.Headers.Add("CustomHeader1", "^.<");
+            request.Headers.Add("CustomHeader2", "=..=");
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var result = await client.SendAsync(request);
+            string resultContent = await result.Content.ReadAsStringAsync();
+            //回覆一樣可以用Model包裝 Json名稱欄位要對傳回值
+            Model Resp = JsonConvert.DeserializeObject<Model>(resultContent);
+            #endregion
+        }
+
+        public class Model
+        {
+            public Model() 
+            {
+                Data = new ModelData();
+                lstData = new List<ModelData>();
+            }
+            public string MProp1 { get; set; }
+            public ModelData Data { get; set; }
+            public string MProp2 { get; set; }
+
+            public List<ModelData> lstData { get; set; }
+        }
+
+        public class ModelData
+        {
+        public string ModelProp1 { get; set; }
+        public string ModelProp2 { get; set; }        
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {     
+            //目標api網址
+            string TargetUri = "";
+            HttpClient client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                //設定為Get
+                Method = HttpMethod.Get,
+                //設定目標網址
+                RequestUri = new Uri(TargetUri)
+            };
+           var result=client.SendAsync(request);
+            //解析為Json 與Post一樣可以包裝至object(這裡沒包)
+            string resultContent = await result.Result.Content.ReadAsStringAsync();
+            
+
+        }
+    }
+}
